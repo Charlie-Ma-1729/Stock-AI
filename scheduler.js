cafc2f136466dd75e4048e2a9a9ab93d8e691324
Charlie-Ma-1729 <717charlie@gmail.com>
2026-05-29 13:57:28 +0800

    path fix

--- scheduler.js
+++ scheduler.js
@@ -15,9 +15,9 @@ function runScrapers() {
     //     return;
     // }
     logger.info('🕷️ 啟動例行性爬蟲任務...');
-    exec('node scrapers/cmoney_scraper.js', (err) => { if (err) logger.error(err.message); });
-    exec('node scrapers/ctee_scraper.js', (err) => { if (err) logger.error(err.message); });
-    exec('node scrapers/udn_scraper.js', (err) => { if (err) logger.error(err.message); });
+    exec('node ./cmoney_scraper.js', (err) => { if (err) logger.error(err.message); });
+    exec('node ./scrapers/ctee_scraper.js', (err) => { if (err) logger.error(err.message); });
+    exec('node ./scrapers/udn_scraper.js', (err) => { if (err) logger.error(err.message); });
 }
 
 /**
